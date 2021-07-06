824e94ea6d1bea7cdc05ea90704cb3572ae2fbdb
Carol-Student22 <carol.lian22@gmail.com>
2021-07-06 11:23:27 -0400

    Default Setup

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,18 +1,20 @@
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 // Uncomment import and local "components" registration if library is not registered globally.
 import { Button } from "@/entry.esm";
+import { ArticlePiece } from "@/entry.esm";
 
 export default Vue.extend({
-  name: 'ServeDev',
+  name: "ServeDev",
   components: {
     Button,
-  }
+  },
 });
 </script>
 
 <template>
   <div id="app">
     <Button />
+    <ArticlePiece />
   </div>
 </template>
---
+++ src/lib-components/ArticlePiece.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>Click me</button>
+</template>
+
+<script>
+export default {
+  name: "ArticlePiece", // vue component name
+  data:
+};
+</script>
+
+<style scoped>
+button {
+  color: blue;
+}
+</style>
