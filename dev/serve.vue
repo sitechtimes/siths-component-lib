126169a214db35d6ac15c15842611a267c8a20fd
Edis Gonuler <edis@gonuler.com>
2021-07-06 09:15:38 -0400

    initialize component library

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+import { Button } from "@/entry.esm";
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Button,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <Button />
+  </div>
+</template>
---
+++ src/lib-components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>Click me</button>
+</template>
+
+<script>
+export default {
+  name: 'Button', // vue component name
+};
+</script>
+
+
+<style scoped>
+button {
+  color: red;
+}
+</style>
