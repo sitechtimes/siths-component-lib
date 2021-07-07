0f8736b7a7ceb816df4e50b06b285c919cf34dae
Carol-Student22 <Carol-Student22@users.noreply.github.com>
2021-07-07 08:34:22 -0400

    Updated Vue file - Need to figure out how to use script

--- src/lib-components/ArticlePiece.vue
+++ src/lib-components/ArticlePiece.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="Test">Test</div>
+  <div class="article flex-row">
+    <div class="flex-col left">
+      <div class="flex-row top-left">
+        <h1 class="category">Category</h1>
+        <h2 class="author-name">Name {{ authorName }}</h2>
+        <h2 class="date">date {{ date }}</h2>
+      </div>
+      <div class="bottom-left">
+        <h3 class="flex-col title">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+          dolor sit amet. {{ title }}
+        </h3>
+      </div>
+    </div>
+
+    <div class="photo" alt="Temp Replacement"></div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ArticlePiece", // vue component
-  prop: {
+  data: {
     authorName: "DANIEL BRISKMAN",
     date: "Jan 21, 2021",
     title: "Test",
@@ -14,7 +30,79 @@ export default {
 </script>
 
 <style scoped>
-.Test {
-  background-color: black;
+@import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@600;700&display=swap");
+
+.article {
+  display: flex;
+  width: 846px;
+  height: 242px;
+}
+.flex-row {
+  display: flex;
+  flex-direction: row;
+}
+.flex-col {
+  flex-direction: column;
+}
+.left {
+  width: 473px;
+  margin: 20px 34px 20px 0;
+}
+
+.top-left {
+  height: 28px;
+  padding-bottom: 20px;
+  display: flex;
+}
+
+.category {
+  background-color: #6638e9;
+  border-radius: 8px;
+  height: 15px;
+  padding: 6px 7px 5px 7px;
+  width: auto;
+  margin: 0 47.62px 0 0;
+
+  color: #fff;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  text-transform: uppercase;
+}
+.author-name {
+  margin: 7px 34px 0 0;
+
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 14px;
+}
+
+.date {
+  margin: 7px 0 0 0;
+  text-align: center;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 14px;
+}
+.title {
+  margin: 0 0 34px 0;
+  font-family: Josefin Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 25px;
+  line-height: 30px;
+}
+
+.photo {
+  width: 339px;
+  height: 242px;
+  border-radius: 15px;
+  border: 1px solid red;
+  background-color: grey;
 }
 </style>
