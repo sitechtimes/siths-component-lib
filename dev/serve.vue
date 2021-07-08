889ddb3338798fa67d904e4df4987eff6514f7a4
popplio2 <danikbrisk@yahoo.com>
2021-07-08 12:21:03 -0400

    Finished see more btn

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,18 +1,18 @@
 <script>
 import Vue from 'vue';
 // Uncomment import and local "components" registration if library is not registered globally.
-import { Button } from "@/entry.esm";
+import { SeeMoreBtn } from "@/entry.esm";
 
 export default Vue.extend({
   name: 'ServeDev',
   components: {
-    Button,
+    SeeMoreBtn
   }
 });
 </script>
 
 <template>
   <div id="app">
-    <Button />
+    <SeeMoreBtn />
   </div>
 </template>
