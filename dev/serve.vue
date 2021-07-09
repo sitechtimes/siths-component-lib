a809129fdaaa0a991788ddb577dec89dc21f912b
Charley13 <charley.baluja22@sitechhs.com>
2021-07-09 11:32:18 -0400

    changed icon documentation

--- dev/serve.vue
+++ dev/serve.vue
@@ -5,30 +5,32 @@ import { CategoryIcon } from "@/entry.esm";
 import { SeeMoreBtn } from "@/entry.esm";
 import AuthorAndDate from "../src/lib-components/AuthorAndDate.vue";
 import { GetNotifiedSection } from "@/entry.esm";
-
 import { SocialMediaIcons } from "@/entry.esm";
 import { ShareIcon } from "@/entry.esm";
+import { Article } from "@/entry.esm";
 export default Vue.extend({
   name: "ServeDev",
   components: {
     CategoryIcon, 
     SeeMoreBtn,
     AuthorAndDate,
-    GetNotifiedSection
+    GetNotifiedSection,
     SocialMediaIcons,
     ShareIcon,
+    Article,
   },
 });
 </script>
 
 <template>
   <div id="app">
-    <category-icon category="opinion"></category-icon>
+    <!-- <category-icon category="opinion"></category-icon>
     <SeeMoreBtn />
     <author-and-date author="Charley Baluja" published="Jan 31, 2020" />
     <GetNotifiedSection />
     <SocialMediaIcons />
-      <ShareIcon />
+      <ShareIcon /> -->
+      <Article />
   </div>
 </template>
 
