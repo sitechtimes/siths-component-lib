6c77e2d2bbb461aa7a512878194d37ab2a88d9cf
popplio2 <danikbrisk@yahoo.com>
2021-07-09 12:48:30 -0400

    Fixed documentation and added media query

--- src/lib-components/FooterSocialMedia.vue
+++ src/lib-components/FooterSocialMedia.vue
@@ -24,13 +24,28 @@ export default {
         text-align: center;
         margin-top: 1.5rem;
     }
+    @media only screen and (max-width: $x-small-screen) {
+        .footer-header {
+            font-size: 2.5rem;
+        }  
+        .footer-social-media-box {
+            width: 30rem;
+            margin: 0 auto;
+            border-bottom: none;
+        }
+    }
 </style>
 
 
 <docs>
 This is the section of the footer with social media icons that, when clicked, take the user to the newspaper's social media pages.
-## Examples
 
+## Variables Used:
+--font: Which controls the font of the page. If for whatever reason the font is changed in a theme, it will change here.
+--primary-color: If a theme were to change this variable it would also change the color of the button.
+$x-small-screen: A media query breakpoint for extra small devices like phones so that the page is responsive. If this variable is changed, the media query breakpoint will change in all other components. This variable is an SCSS variable because CSS custom properties do not work in media queries.
+
+## Examples
 ```jsx
 <FooterSocialMedia />
 ```
