76aad8e2d0ccb941cf294b8f4c34f13e35ffad68
Ehan <ethanliang405@gmail.com>
2021-07-09 11:33:27 -0400

    update styles

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,6 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export { default as Sidebar } from "./Sidebar.vue";
-export { default as SidebarContainer } from "./SidebarContainer.vue";
-export { default as CategoryIcon } from "./CategoryIcon.vue";
-export { default as SeeMoreBtn } from "./SeeMoreBtn.vue";
 export { default as AuthorAndDate } from "./AuthorAndDate.vue";
