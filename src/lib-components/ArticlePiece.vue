d0e81ae81e30943cd47abd65de53cf8bc986dffb
Carol-Student22 <carol.lian22@gmail.com>
2021-07-12 08:04:47 -0400

    .

--- src/lib-components/ArticlePiece.vue
+++ src/lib-components/ArticlePiece.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="article">
     <div class="text-box flex-direction-column">
-
       <div class="text-box-top flex-direction-row">
-        <h5 class="category">{{category}}</h5>
-        <h5 class="author author-overflow">{{author}}</h5>
-        <h5 class="date">{{date}}</h5>
+        <h5 class="category">{{ category }}</h5>
+        <h5 class="author author-overflow">{{ author }}</h5>
+        <h5 class="date">{{ date }}</h5>
       </div>
 
-      <div class="text-box-btm ">
-        <p class="title title-overflow">{{title}}</p>
+      <div class="text-box-btm">
+        <p class="title title-overflow">{{ title }}</p>
       </div>
-
     </div>
     <div class="img" alt="Temp Replacement"></div>
-
   </div>
 </template>
 
@@ -30,26 +27,25 @@ export default {
 };
 </script>
 
-
 <style lang="scss">
 :root {
   --filler: var(--grey);
   --imgSmallWidth: 124px;
   --imgSmallHeight: 101px;
-  --imgLargeWidth: 351px ;
+  --imgLargeWidth: 351px;
   --imgLargeHeight: 231px;
-  --imgSmallBorderRadius:  6.27423px;
+  --imgSmallBorderRadius: 6.27423px;
   --imgLargeBorderRadius: 15px;
 
-  --fontSmallScreenSmall:8px;
-  --fontSmallScreenLarge:14px; 
+  --fontSmallScreenSmall: 8px;
+  --fontSmallScreenLarge: 14px;
   --fontLargeScreenSmall: 14px;
-  --fontLargeScreenLarge: 25px ;
+  --fontLargeScreenLarge: 25px;
 
-  --textBoxTopHeightSmall:13.52px ;
+  --textBoxTopHeightSmall: 13.52px;
   --textBoxTopHeightLarge: 24.39px;
 
-  --textBoxBottomHeightSmall:49px;
+  --textBoxBottomHeightSmall: 49px;
   --textBoxBottomWidthSmall: 225px;
   --textBoxBottomHeightLarge: 97px;
   --textBoxBottomWidthLarge: 443px;
@@ -76,8 +72,8 @@ body {
   height: var(--imgSmallHeight);
   border-radius: var(--imgSmallBorderRadius);
 }
-@media only screen and (min-width: $large-screen){
-  .img{
+@media only screen and (min-width: $large-screen) {
+  .img {
     width: var(--imgLargeWidth);
     height: var(--imgKLargeHeight);
     border-radius: var(--imgLargeBorderRadius);
@@ -95,7 +91,7 @@ body {
     width: 846px;
     height: 242px;
     flex-direction: row-reverse !important;
-}
+  }
 }
 .text-box {
   margin: 21px 14px 10px 0;
@@ -103,17 +99,17 @@ body {
 @media only screen and (min-width: $large-screen) {
   .text-box {
     margin: 25px 0 0 51px;
-    width:443px;
+    width: 443px;
   }
 }
 
-
 .text-box-top {
-  margin-bottom: 7.48px ;
+  margin-bottom: 7.48px;
 }
 
-
-.category, .author, .date {
+.category,
+.author,
+.date {
   font-family: var(--font);
   font-size: var(--fontSmallScreenSmall);
   line-height: var(--fontSmallScreenSmall);
@@ -121,33 +117,33 @@ body {
 
   padding-top: 3.86px;
 }
-@media only screen and (min-width: $large-screen){
-  .category, .author, .date {
-  font-size: var(--fontLargeScreenSmall);
-  line-height: var(--fontLargeScreenSmall);
-  
+@media only screen and (min-width: $large-screen) {
+  .category,
+  .author,
+  .date {
+    font-size: var(--fontLargeScreenSmall);
+    line-height: var(--fontLargeScreenSmall);
 
-  padding-top: 6.97px ;
-}
+    padding-top: 6.97px;
+  }
 }
 
 .category {
-  background-color: var(--opinion) ;
+  background-color: var(--opinion);
   color: var(--white);
 
   padding: 3.86px 3.96px 2.9px 3.96px;
   border-radius: 7px;
-  margin-right:31.01px;
+  margin-right: 31.01px;
   height: 13.52px;
 }
-@media only screen and (min-width: $large-screen){
+@media only screen and (min-width: $large-screen) {
   .category {
     margin-right: 39px;
     padding: 6.97px 7.61px 5.23px 7.61px;
     border-radius: 8px;
 
     height: 24.39px;
-    
   }
 }
 
@@ -160,30 +156,28 @@ body {
   text-overflow: ellipsis;
 }
 
-
 .author {
   margin-right: 9.16px;
   width: 82px;
 }
-@media only screen and (min-width: $large-screen){
+@media only screen and (min-width: $large-screen) {
   .author {
     width: 156px;
     margin-right: 22px;
   }
 }
 
-
 .text-box-btm {
   height: var(--textBoxBottomHeightSmall);
-  width: var(--textBoxBottomWidthSmall)
+  width: var(--textBoxBottomWidthSmall);
 }
-@media only screen and (min-width: $large-screen){
+@media only screen and (min-width: $large-screen) {
   .text-box-btm {
     height: var(--textBoxBottomHeightLarge);
-  width: var(--textBoxBottomWidthlarge)
+    width: var(--textBoxBottomWidthlarge);
   }
 }
-.title-overflow{
+.title-overflow {
   display: block;
   display: -webkit-box;
   -webkit-line-clamp: 3; //The Number of Lines Shown Before Cutting Off the Text
@@ -191,7 +185,7 @@ body {
   overflow: hidden;
   text-overflow: ellipsis;
 }
-@media only screen and (min-width: $large-screen){
+@media only screen and (min-width: $large-screen) {
   .text-overflow {
     -webkit-line-clamp: 5;
   }
@@ -199,15 +193,14 @@ body {
 .title {
   font-family: var(--font);
   font-size: var(--fontSmallScreenLarge);
-  line-height: 15px ;
+  line-height: 15px;
   font-weight: bold;
 }
-@media only screen and (min-width: $large-screen){
+@media only screen and (min-width: $large-screen) {
   .title {
     font-size: var(--fontLargeScreenLarge);
-  line-height: 30px ;
-  font-weight: bold;
+    line-height: 30px;
+    font-weight: bold;
   }
 }
-  
 </style>
