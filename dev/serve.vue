d064d6fb8a1394dca8eca06dfbb2f51bdf2099af
alstnc <alston.chan22@sitechhs.com>
2021-07-12 10:08:32 -0400

    fixed da stuff

--- dev/serve.vue
+++ dev/serve.vue
@@ -31,6 +31,7 @@ export default Vue.extend({
 
 <template>
   <div id="app">
+    <Datebar />
     <category-icon category="opinion"></category-icon>
     <SeeMoreBtn />
     <author-and-date author="Charley Baluja" published="Jan 31, 2020" />
@@ -40,7 +41,6 @@ export default Vue.extend({
       <EntertainmentSideArticle />
     <FooterSocialMedia />
     <Navbar />
-    <Datebar />
     <text-below-article-preview articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Daniel Briskman" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="I hate rockets, and this is why you should too" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"/>
   </div>
 </template>
