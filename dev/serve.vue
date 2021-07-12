a168ea6dad5a36c5d2c698b101b787c96cd3706c
alstnc <alston.chan22@sitechhs.com>
2021-07-12 09:03:07 -0400

    Revert "Revert "Merge branch 'develop' of https://github.com/sitechtimes/siths-component-lib into develop""
    This reverts commit 8b575cb2d6e727b53759a0aad523a2a37c4a6731.

--- dev/serve.vue
+++ dev/serve.vue
@@ -2,11 +2,24 @@
 import Vue from "vue";
 // Uncomment import and local "components" registration if library is not registered globally.
 import { CategoryIcon } from "@/entry.esm";
-
+import { SeeMoreBtn } from "@/entry.esm";
+import AuthorAndDate from "../src/lib-components/AuthorAndDate.vue";
+import { GetNotifiedSection } from "@/entry.esm";
+import { SocialMediaIcons } from "@/entry.esm";
+import { ShareIcon } from "@/entry.esm";
+import { EntertainmentSideArticle } from "@/entry.esm";
+import { FooterSocialMedia } from "@/entry.esm";
 export default Vue.extend({
   name: "ServeDev",
   components: {
-    CategoryIcon,
+    CategoryIcon, 
+    SeeMoreBtn,
+    AuthorAndDate,
+    GetNotifiedSection,
+    SocialMediaIcons,
+    ShareIcon,
+    EntertainmentSideArticle,
+    FooterSocialMedia,
   },
 });
 </script>
@@ -14,6 +27,14 @@ export default Vue.extend({
 <template>
   <div id="app">
     <category-icon category="opinion"></category-icon>
+    <SeeMoreBtn />
+    <author-and-date author="Charley Baluja" published="Jan 31, 2020" />
+    <GetNotifiedSection />
+    <SocialMediaIcons />
+      <ShareIcon /> 
+      <EntertainmentSideArticle />
+    <FooterSocialMedia />
+    <Navbar />
   </div>
 </template>
 
