62451be9b06daf27a5035570fd7736f7edfd208e
Charley13 <charley.baluja22@sitechhs.com>
2021-07-13 18:32:58 -0400

    innn progress

--- src/lib-components/OpinionSlider.vue
+++ src/lib-components/OpinionSlider.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="big">
+  <div class="opinion-section">
       <div class="scrolls">
       <text-below-article-preview width="" class="opinion-card" articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Daniel Briskman" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="I hate rockets, and this is why you should too" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"></text-below-article-preview>
         <text-below-article-preview class="opinion-card" articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Alston Chan" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="Rockets are so valid and chill" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"></text-below-article-preview>
           <text-below-article-preview class="opinion-card" articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Charley Baluja" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="Rockets are cool" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"></text-below-article-preview>
             </div>
-            <div class="hhh">
-                <span class="scroll-bar"></span>
-                <span class="scroll-circle"></span>
-            </div>
+      <!-- <div class="span-div">
+                <span id="1" class="scrollbar" @click="hello"> </span>
+                    <span id="2" class="scroll-circle" @click="hi"></span>
+         </div> -->
 
  </div>
 </template>
@@ -16,49 +16,67 @@
 <script>
 
 export default {
- name: "OpinionSlider"
-
+ name: "OpinionSlider",
+methods: {
+    // hello: function(){
+    // const scrollbar = document.getElementById("1")
+    // scrollbar.classList.toggle("scroll-circle")
+    // console.log(scrollbar.classList)
+    // },
+    // hi: function(){
+    // const scrollCircle = document.getElementById("2")
+    // scrollCircle.classList.toggle("scrollbar")
+    // scrollCircle.classList.remove("scroll-circle")
+    // console.log(scrollCircle.classList);
+    // }
+}
 }
 </script>
 
 <style>
-.big{
+.opinion-section{
     display: flex;
     flex-direction: column;
+      margin-bottom: 3rem;
 }
-.big span {
-    margin: auto;
+.span-div{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
 }
 .scrolls{
     display: flex;
     flex-direction: row;
-    width: 80vw;
-margin: 3rem auto;
-overflow-x: auto;
--ms-overflow-style: none;
-   scrollbar-width: none;
+    width: 70vw;
+    margin: 3rem auto;
+    overflow-x: auto;
 }
 .opinion-card{
     margin-right: 1rem;
 }
-::-webkit-scrollbar{
-   display: none;
-}
-.scroll-bar{
-width: 5rem;
+.scrolls::-webkit-scrollbar{
 height: 1.5rem;
-background-color: var(--primary-color);
+border-radius: 1.5rem; 
+background-color: var(--accent-color);
+width: 20rem;
+}
+.scrolls::-webkit-scrollbar-thumb{
+  background-color: var(--primary-color);
+  height: 1.5rem;
+border-radius: 1.5rem; 
+width: 10rem;
+}
+.scrollbar{
+width: 70vw;
+height: 1.5rem;
+background-color: var(--accent-color);
 border-radius: 1.5rem;
 }
-.scroll-circle{
+/* .scroll-circle{
     width: 1.5rem;
     height: 1.5rem;
     margin-left: 1rem;
     border-radius: 1.5rem;
     background-color: var(--accent-color);
-}
-
-.hhh {
-    margin: 0px;
-}
+} */
 </style>
