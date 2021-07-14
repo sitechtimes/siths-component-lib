847d0dfcbf63a787482a697876db26630b69e4dc
Charley13 <71568725+Charley13@users.noreply.github.com>
2021-07-14 08:48:43 -0400

    slider done (minus media queries cause I can't do those until I get new data from the dev branch)

--- src/lib-components/OpinionSlider.vue
+++ src/lib-components/OpinionSlider.vue
@@ -5,10 +5,6 @@
         <text-below-article-preview class="opinion-card" articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Alston Chan" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="Rockets are so valid and chill" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"></text-below-article-preview>
           <text-below-article-preview class="opinion-card" articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Charley Baluja" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="Rockets are cool" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"></text-below-article-preview>
             </div>
-      <!-- <div class="span-div">
-                <span id="1" class="scrollbar" @click="hello"> </span>
-                    <span id="2" class="scroll-circle" @click="hi"></span>
-         </div> -->
 
  </div>
 </template>
@@ -17,19 +13,6 @@
 
 export default {
  name: "OpinionSlider",
-methods: {
-    // hello: function(){
-    // const scrollbar = document.getElementById("1")
-    // scrollbar.classList.toggle("scroll-circle")
-    // console.log(scrollbar.classList)
-    // },
-    // hi: function(){
-    // const scrollCircle = document.getElementById("2")
-    // scrollCircle.classList.toggle("scrollbar")
-    // scrollCircle.classList.remove("scroll-circle")
-    // console.log(scrollCircle.classList);
-    // }
-}
 }
 </script>
 
@@ -50,15 +33,15 @@ methods: {
     width: 70vw;
     margin: 3rem auto;
     overflow-x: auto;
+    overflow-y: hidden;
 }
 .opinion-card{
     margin-right: 1rem;
 }
 .scrolls::-webkit-scrollbar{
-height: 1.5rem;
+height: 0.8rem;
 border-radius: 1.5rem; 
 background-color: var(--accent-color);
-width: 20rem;
 }
 .scrolls::-webkit-scrollbar-thumb{
   background-color: var(--primary-color);
@@ -72,11 +55,19 @@ height: 1.5rem;
 background-color: var(--accent-color);
 border-radius: 1.5rem;
 }
-/* .scroll-circle{
-    width: 1.5rem;
-    height: 1.5rem;
-    margin-left: 1rem;
-    border-radius: 1.5rem;
-    background-color: var(--accent-color);
-} */
 </style>
+
+<docs>
+This element is a slider used in the opinion section of our website. It displays part of an article and invites the user to scroll to see the other two articles. 
+
+## Variables Used:
+--primary-color
+--acent-color
+
+## Examples
+
+```jsx
+ <OpinionSlider />
+```
+
+</docs>
