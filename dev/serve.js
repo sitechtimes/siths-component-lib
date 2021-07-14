ec598cafa9b5454d157cf3539f2217e9facc3e53
alston <alston.chan22@sitechhs.com>
2021-07-14 09:01:37 -0400

    replaced MOMENT.JS with DATE-FNS (has tree shaking!)

--- dev/serve.js
+++ dev/serve.js
@@ -13,10 +13,6 @@ library.add(faCalendar);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
-import moment from "moment";
-
-Vue.prototype.moment = moment;
-
 // To register individual components where they are used (serve.vue) instead of using the
 // library as a whole, comment/remove this import and it's corresponding "Vue.use" call
 import SithsComponentLib from "@/entry.esm";
@@ -29,6 +25,9 @@ library.add(faTwitter);
 library.add(faFacebook);
 library.add(faShare);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+import { createDateFilter } from "vue-date-fns";
+Vue.filter("date", createDateFilter("LLLL do, yyyy"));
 
 new Vue({
   render: (h) => h(Dev),
