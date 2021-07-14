864a392f1fcace6273c7577debb364fa11997520
Ehan <ethanliang405@gmail.com>
2021-07-14 08:39:27 -0400

    added missing sidebar component from serve.vue

--- dev/serve.vue
+++ dev/serve.vue
@@ -24,8 +24,8 @@ export default Vue.extend({
   components: {
     SidebarContainer,
     CategoryIconSidebar,
-    AuthorAndDateSidebar,     
-    CategoryIcon, 
+    AuthorAndDateSidebar,
+    CategoryIcon,
     SeeMoreBtn,
     AuthorAndDate,
     GetNotifiedSection,
@@ -40,7 +40,7 @@ export default Vue.extend({
     CategoryIcon,
     Footer,
     Sidebar,
-    TextBelowArticlePreview
+    TextBelowArticlePreview,
   },
 });
 </script>
@@ -50,31 +50,46 @@ export default Vue.extend({
     <!-- <category-icon-sidebar category="opinion" />
     <author-and-date-sidebar author="Charley Baluja" published="Jan 31, 2020" />  -->
     <Datebar />
+    <sidebar-container />
     <category-icon category="opinion"></category-icon>
     <SeeMoreBtn />
     <author-and-date author="Charley Baluja" published="Jan 31, 2020" />
     <GetNotifiedSection />
     <SocialMediaIcons />
-      <ShareIcon /> 
+    <ShareIcon />
     <FooterSocialMedia />
     <Navbar />
     <!-- <text-below-article-preview articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Daniel Briskman" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="I hate rockets, and this is why you should too" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"/> -->
     <Datebar />
-    <text-below-article-preview articleUrl="https://theconversation.com/us/topics/rocket-science-195" author="Daniel Briskman" published="Jan 31, 2020" category="opinion" imageAlt="Space X Rocket" title="I hate rockets, and this is why you should too" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"/>
+    <text-below-article-preview
+      articleUrl="https://theconversation.com/us/topics/rocket-science-195"
+      author="Daniel Briskman"
+      published="Jan 31, 2020"
+      category="opinion"
+      imageAlt="Space X Rocket"
+      title="I hate rockets, and this is why you should too"
+      imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"
+    />
     <FooterNav />
-      <EntertainmentSideArticle articleUrl="https://theconversation.com/us/topics/rocket-science-195" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU" category="entertainment" author="Charley Baluja" published="July 12, 2021" title="Podcasts: Radio, But Less Cool"/>
+    <EntertainmentSideArticle
+      articleUrl="https://theconversation.com/us/topics/rocket-science-195"
+      imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNAu-JntINSfv0U6b2Df439C-cXbDOOYRzsj9UuhMwwP290pnObcSbtJHXo93jNBpA5Ys&usqp=CAU"
+      category="entertainment"
+      author="Charley Baluja"
+      published="July 12, 2021"
+      title="Podcasts: Radio, But Less Cool"
+    />
     <Footer />
     <category-icon-sidebar category="opinion" />
-    <author-and-date-sidebar author="Charley Baluja" published="Jan 31, 2020" /> 
+    <author-and-date-sidebar author="Charley Baluja" published="Jan 31, 2020" />
 
-    <author-and-date author='Regular' published='Regular 31, 2020' /> 
-    <category-icon category='entertainment'/>
+    <author-and-date author="Regular" published="Regular 31, 2020" />
+    <category-icon category="entertainment" />
     <Navbar />
   </div>
 </template>
 
 <style lang="scss">
-
 @media only screen and (min-width: $x-large-screen) {
 }
 
@@ -86,7 +101,7 @@ export default Vue.extend({
   }
 }
 
-@media only screen and  (max-width: $large-screen) {
+@media only screen and (max-width: $large-screen) {
   *,
   html,
   body {
@@ -95,11 +110,8 @@ export default Vue.extend({
 }
 
 @media only screen and (max-width: $mid-screen) {
-
 }
 
-@media only screen and (max-width: $x-small-screen)  {
+@media only screen and (max-width: $x-small-screen) {
 }
-
-
 </style>
